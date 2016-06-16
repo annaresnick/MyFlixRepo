4f7eba6a6acb85eb8f6c5728f4d8bb163a741661
Anna Resnick <aresnick@fb.com>
2016-06-15 17:21:31 -0700

    First commit

---
+++ app/src/main/java/com/example/aresnick/flixster/MoviesActivity.java
@@ -0,0 +1,77 @@
+package com.example.aresnick.flixster;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.example.aresnick.flixster.models.Movie;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MoviesActivity extends AppCompatActivity {
+
+    ArrayList<Movie> movies;
+    MovieArrayAdapter movieAdapter;
+    ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+
+        lvItems = (ListView) findViewById(R.id.lvMovies);
+        movies = new ArrayList<>();
+        movieAdapter = new MovieArrayAdapter(this, movies);
+        lvItems.setAdapter(movieAdapter);
+
+        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(url, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                JSONArray movieJsonResults = null;
+
+                try {
+                    movieJsonResults = response.getJSONArray("results");
+                    movies.addAll(Movie.fromJSONArray(movieJsonResults));
+                    movieAdapter.notifyDataSetChanged();
+                    Log.d("DEBUG", movies.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
+
+/*        // 1. Get the actual movies
+        ArrayList<Movie> movies = Movie.getFakeMovies();
+
+        // 2. Get the ListView that we want to populate
+        ListView lvMovies = (ListView)findViewById(R.id.lvMovies);
+
+        // 3. Create an ArrayAdapter
+        //ArrayAdapter<Movie> adapter = new ArrayAdapter<Movie>(this, android.R.layout.simple_list_item_1, movies);
+        MoviesAdapter adapter = new MoviesAdapter(this, movies);
+
+
+        // 4. Associate the adapter with the ListView
+        if (lvMovies != null) {
+            lvMovies.setAdapter(adapter);
+        }*/
+    }
+}
---
+++ app/src/main/java/com/example/aresnick/flixster/models/Movie.java
@@ -0,0 +1,80 @@
+package com.example.aresnick.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by aresnick on 6/15/2016.
+ */
+public class Movie {
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    String posterPath;
+    String originalTitle;
+    String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException{
+        this.posterPath = jsonObject.getString("poster path");
+        this.originalTitle = jsonObject.getString("original title");
+        this.overview = jsonObject.getString("overview");
+    }
+
+    public static ArrayList<Movie> fromJSONArray(JSONArray array){
+        ArrayList<Movie> results = new ArrayList<>();
+
+        for (int x = 0; x < array.length(); x++) {
+            try {
+                results.add(new Movie(array.getJSONObject(x)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return results;
+    }
+}
+//package com.example.aresnick.flixster;
+//
+///**
+// * Created by aresnick on 6/15/2016.
+// */
+//public class Movie {
+//    public String title;
+//    public String posterUrl;
+//    public int rating;
+//
+//    public Movie(String title, String posterUrl, int rating) {
+//        this.title = title;
+//        this.posterUrl = posterUrl;
+//        this.rating = rating;
+//    }
+//
+//    public static ArrayList<Movie> getFakeMovies(){
+//        ArrayList<Movie> movies = new ArrayList<>();
+//
+//        for(int i = 0; i < 60; i++) {
+//            movies.add(new Movie("The Social Network", "", 75));
+//            movies.add(new Movie("The Internship", "", 50));
+//            movies.add(new Movie("The Lion King", "", 100));
+//        }
+//        return movies;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return title + " - " + rating;
+//    }
+//}
